e63b82afa0a50499a7d593bfabd95f4f9375e7a1
Zachary Cipolletta <ZackCipolletta@gmail.com>
2023-01-20 12:37:20 -0800

    begin adding html elements, css styling and first functions to handle user interaction

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,29 @@ function beepBoop(input) {
   let returnArr = [];
   for(let i = 0; i <= userNum; i++) {  // adds value of i onto returnArr array
     returnArr.push(i); 
-  };
+  }
   returnArr.forEach(function(element, i){ // runs a loop for each value in the return array
     let elToString = element.toString(); // converts element of the array to a string
     if(elToString.includes(3)) { // checks if string element contains a 3
-      returnArr.splice(i, 1, "Won't you be my neighbor?") // if the string does contain the specified value (in this case 3), the function will then remove 1 element at index position i and replace it with the given phrase. 
+      returnArr.splice(i, 1, "Won't you be my neighbor?"); // if the string does contain the specified value (in this case 3), the function will then remove 1 element at index position i and replace it with the given phrase. 
     } else if(elToString.includes(2)) { // checks if string element contains a 2
-      returnArr.splice(i, 1, 'Boop')
+      returnArr.splice(i, 1, 'Boop');
     } else if(elToString.includes(1)) {  // checks if string element contains a 1
-      returnArr.splice(i, 1, 'Beep')
+      returnArr.splice(i, 1, 'Beep');
     }
   });
   console.log(returnArr);
-}  
+}
+
+
+function submitFunc(e){
+  e.preventDefault();
+
+  
+
+};
+
+
+window.addEventListener("load", function(){
+  document.querySelector("button").addEventListener("click", submitFunc);
+});
