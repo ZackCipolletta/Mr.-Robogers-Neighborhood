b0dcda4d004b0eedaf11c7be2ebf0df241864f4b
Zachary Cipolletta <ZackCipolletta@gmail.com>
2023-01-20 09:42:01 -0800

    return a string between 0 and a given number, replacing the number 1 with the string 'Beep'

--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,9 @@ function beepBoop(input) {
   for(let i = 0; i <= userNum; i++) {
     returnArr.push(i);
   };
+  if(returnArr.includes(1)){
+    let indexOf1 = returnArr.indexOf(1);
+    returnArr.splice(indexOf1, 1, 'Beep');
+  }
   console.log(returnArr);
 }
