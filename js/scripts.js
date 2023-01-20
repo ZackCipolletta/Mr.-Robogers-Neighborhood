7c8d6e6ab115018168dd2162028d53e1b1d695a9
Zachary Cipolletta <ZackCipolletta@gmail.com>
2023-01-20 10:54:36 -0800

    remove commented out code

--- js/scripts.js
+++ js/scripts.js
@@ -12,22 +12,3 @@ function beepBoop(input) {
   });
   console.log(returnArr);
 }  
-
-
-//  function test2() { // returns an array containing string values of each number split into single digits. EX: [21, 22, 23] becomes ['2', '1', '2', '2', '2', '3'].
-//   let testArr = [21, 22, 23];
-//   let testArrToString = testArr.toString();
-//   let testArrToStringSplit = testArrToString.split('');
-//   console.log(testArrToStringSplit);
-//  }
-
-
-
-
-// function test(){
-// let testArr = [21];
-// let testArrSplit = testArr.split();
-//   if(testArrSplit.includes(1)){
-//     console.log('testArr includes a 1')
-//   };
-// }
