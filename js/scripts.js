fc06e35bc29e7459dca4e74ba0f17bb873f10f45
Zachary Cipolletta <ZackCipolletta@gmail.com>
2023-01-20 11:09:59 -0800

    replace each number containing 1 with 'Beep', unless the number contains 2 which is replaced with 'Boop'

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,34 @@
 function beepBoop(input) {
-  let userNum = parseInt(input);
+  let userNum = parseInt(input); 
   let returnArr = [];
   for(let i = 0; i <= userNum; i++) {
-    returnArr.push(i);
+    returnArr.push(i); 
   };
-  returnArr.forEach(function(element, i){
-    let elToString = element.toString();
-    if(elToString.includes(1)) {
+  returnArr.forEach(function(element, i){ 
+    let elToString = element.toString(); 
+    if(elToString.includes(2)) {
+      returnArr.splice(i, 2, 'Boop')
+    } else if(elToString.includes(1)) { 
       returnArr.splice(i, 1, 'Beep')
     }
   });
   console.log(returnArr);
 }  
+
+
+
+
+// function beepBoop(input) {
+//   let userNum = parseInt(input); // converts string input to a number
+//   let returnArr = [];
+//   for(let i = 0; i <= userNum; i++) {
+//     returnArr.push(i); // adds value of i onto returnArr array
+//   };
+//   returnArr.forEach(function(element, i){ // runs a loop for each value in the return array
+//     let elToString = element.toString(); // converts element of the array to a string
+//     if(elToString.includes(1)) { // checks if string element contains a 1
+//       returnArr.splice(i, 1, 'Beep') // if string element does contain a 1, replaces the value of that element at index position i with the string 'Beep'
+//     }
+//   });
+//   console.log(returnArr);
+// }  
