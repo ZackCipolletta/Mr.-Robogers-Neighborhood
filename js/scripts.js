8e556504754fb11b96ed94ab0ff4533a443cb749
Zachary Cipolletta <ZackCipolletta@gmail.com>
2023-01-20 10:52:46 -0800

    return every number between 0 and a given number, replacing each number containing a 1 with the word 'Beep'

--- js/scripts.js
+++ js/scripts.js
@@ -4,14 +4,15 @@ function beepBoop(input) {
   for(let i = 0; i <= userNum; i++) {
     returnArr.push(i);
   };
-  returnArr.forEach(function(element){
-    if(returnArr.includes(1)){
-      let indexOf1 = returnArr.indexOf(1);
-      returnArr.splice(indexOf1, 1, 'Beep');
+  returnArr.forEach(function(element, i){
+    let elToString = element.toString();
+    if(elToString.includes(1)) {
+      returnArr.splice(i, 1, 'Beep')
     }
   });
   console.log(returnArr);
 }  
+
 
 //  function test2() { // returns an array containing string values of each number split into single digits. EX: [21, 22, 23] becomes ['2', '1', '2', '2', '2', '3'].
 //   let testArr = [21, 22, 23];
