0497612422542c5e750977422f24fe457c17a4aa
Zachary Cipolletta <ZackCipolletta@gmail.com>
2023-01-20 11:33:28 -0800

    add comments to code describing what is happening at each line

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,18 @@
 function beepBoop(input) {
-  let userNum = parseInt(input); 
+  let userNum = parseInt(input); // converts string input to a number
   let returnArr = [];
-  for(let i = 0; i <= userNum; i++) {
+  for(let i = 0; i <= userNum; i++) {  // adds value of i onto returnArr array
     returnArr.push(i); 
   };
-  returnArr.forEach(function(element, i){ 
-    let elToString = element.toString(); 
-    if(elToString.includes(3)) {
-      returnArr.splice(i, 1, "Won't you be my neighbor?")
-    } else if(elToString.includes(2)) {
+  returnArr.forEach(function(element, i){ // runs a loop for each value in the return array
+    let elToString = element.toString(); // converts element of the array to a string
+    if(elToString.includes(3)) { // checks if string element contains a 3
+      returnArr.splice(i, 1, "Won't you be my neighbor?") // if the string does contain the specified value (in this case 3), the function will then remove 1 element at index position i and replace it with the given phrase. 
+    } else if(elToString.includes(2)) { // checks if string element contains a 2
       returnArr.splice(i, 1, 'Boop')
-    } else if(elToString.includes(1)) { 
+    } else if(elToString.includes(1)) {  // checks if string element contains a 1
       returnArr.splice(i, 1, 'Beep')
     }
   });
   console.log(returnArr);
 }  
-
-
-
-
-// function beepBoop(input) {
-//   let userNum = parseInt(input); // converts string input to a number
-//   let returnArr = [];
-//   for(let i = 0; i <= userNum; i++) {
-//     returnArr.push(i); // adds value of i onto returnArr array
-//   };
-//   returnArr.forEach(function(element, i){ // runs a loop for each value in the return array
-//     let elToString = element.toString(); // converts element of the array to a string
-//     if(elToString.includes(1)) { // checks if string element contains a 1
-//       returnArr.splice(i, 1, 'Beep') // if string element does contain a 1, replaces the value of that element at index position i with the string 'Beep'
-//     }
-//   });
-//   console.log(returnArr);
-// }  
