d69fba44f5fe7b430cc03b6789686d10727158ee
Zachary Cipolletta <ZackCipolletta@gmail.com>
2023-01-20 16:01:09 -0800

    add a name input field and add user's name to the string 'won't you be my neighbor?'

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-function beepBoop(input) {
+function beepBoop(input, uName) {
   let userNum = parseInt(input); // converts string input to a number
   let returnArr = [];
   for(let i = 0; i <= userNum; i++) {  // adds value of i onto returnArr array
@@ -7,7 +7,7 @@ function beepBoop(input) {
   returnArr.forEach(function(element, i){ // runs a loop for each value in the return array
     let elToString = element.toString(); // converts element of the array to a string
     if(elToString.includes(3)) { // checks if string element contains a 3
-      returnArr.splice(i, 1, "Won't you be my neighbor?"); // if the string does contain the specified value (in this case 3), the function will then remove 1 element at index position i and replace it with the given phrase. 
+      returnArr.splice(i, 1, "Won't you be my neighbor " + uName +" ?"); // if the string does contain the specified value (in this case 3), the function will then remove 1 element at index position i and replace it with the given phrase. 
     } else if(elToString.includes(2)) { // checks if string element contains a 2
       returnArr.splice(i, 1, 'Boop');
     } else if(elToString.includes(1)) {  // checks if string element contains a 1
@@ -19,6 +19,8 @@ function beepBoop(input) {
 
 function clearFunc(){
   const divEl = document.querySelector('.results');
+  document.getElementById('nameInput').value = '';
+  document.getElementById('beepBoopInput').value = '';
   divEl.innerText = '';
 }
 
@@ -30,9 +32,15 @@ function displayToPage(el) {
   return  divEl;
 }
 
+function userName() {
+  let uName = document.getElementById('nameInput').value;
+  uName = uName.trim();
+  return uName;
+}
+
 function reverseEl() {
-  const userInput = document.querySelector('input').value;
-  let toBeDisplayed = beepBoop(userInput);
+  const userInput = document.getElementById('beepBoopInput').value;
+  let toBeDisplayed = beepBoop(userInput, userName());
   let newP = document.createElement('p');
   const divEl = document.querySelector('.results');
   const reverseEl = toBeDisplayed.reverse();
@@ -42,8 +50,9 @@ function reverseEl() {
 }
 
 function submitFunc(){
-   const userInput = document.querySelector('input').value;
-  let toBeDisplayed = beepBoop(userInput);
+  userName();
+  const userInput = document.getElementById('beepBoopInput').value;
+  let toBeDisplayed = beepBoop(userInput, userName());
   displayToPage(toBeDisplayed);
 }
 
