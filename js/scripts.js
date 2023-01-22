76c2b1a36ef6dd868c1925adeefb693a53c0d157
Zachary Cipolletta <ZackCipolletta@gmail.com>
2023-01-21 17:31:01 -0800

    add ability for beepBoop to handle an empty string passed as user name

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,7 @@ function beepBoop(input, uName) {
   returnArr.forEach(function(element, i){ // runs a loop for each value in the return array
     let elToString = element.toString(); // converts element of the array to a string
     if(elToString.includes(3)) { // checks if string element contains a 3
-      returnArr.splice(i, 1, "Won't you be my neighbor" + uName +"?"); // if the string does contain the specified value (in this case 3), the function will then remove 1 element at index position i and replace it with the given phrase. 
+      returnArr.splice(i, 1, "Won't you be my neighbor" + uName + "?"); // if the string does contain the specified value (in this case 3), the function will then remove 1 element at index position i and replace it with the given phrase. 
     } else if(elToString.includes(2)) { // checks if string element contains a 2
       returnArr.splice(i, 1, 'Boop');
     } else if(elToString.includes(1)) {  // checks if string element contains a 1
@@ -35,6 +35,10 @@ function displayToPage(el) {
 function userName() {
   let uName = document.getElementById('nameInput').value;
   uName = uName.trim().toLowerCase().charAt(0).toUpperCase() + uName.slice(1);
+  uName = uName.trim();
+  if(uName !== '') {
+    uName = ' ' + uName;
+   }
   return uName;
 }
 
